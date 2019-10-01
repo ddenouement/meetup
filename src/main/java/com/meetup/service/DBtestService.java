26da2715c3a7c3f726418c0ef076132b557eb6f5
ddenouement <julialex38@gmail.com>
2019-10-01 23:49:54 +0300

    DB commit 3nd try(test fixed). All comments drom previous commits are taken into account (lombok lib; increased varchar length in Schema; added new folders; placed parametrized queries to .properties file

--- src/main/java/com/meetup/service/DBtestService.java
+++ src/main/java/com/meetup/service/DBtestService.java
@@ -25,29 +25,29 @@ public class DBtestService {
 
         Speaker a0 = new Speaker ();
         a0.setPassword("123");
-        a0.setEmail("ppp@gmail.com");
+        a0.setEmail("ssss@gmail.com");
         a0.setNativeLanguage("english");
         a0.setName("Eugenie");
         a0.setSurname("Pslsl");
-        a0.setLogin("ppPp");
+        a0.setLogin("g");
 
         Listener a1 = new Listener ();
         a1.setPassword("kddkdk");
-        a1.setEmail("qw@gmail.com");
-        a1.setLogin("qw");
+        a1.setEmail("y@gmail.com");
+        a1.setLogin("q");
 
         Listener a2 = new Listener();
         a2.setPassword("gh");
-        a2.setEmail("gh@gmail.com");
-        a2.setLogin("gh");
+        a2.setEmail("t@gmail.com");
+        a2.setLogin("w");
 
         Listener a3 = new Listener ();
         a3.setPassword("38282");
-        a3.setEmail("ldldl@gmail.com");
-        a3.setLogin("pnmd");
- speakersRepository.insertSpeaker(a0);
-           listenersRepository.insertListener(a1);
-           listenersRepository.insertListener(a2);
+        a3.setEmail("r@gmail.com");
+        a3.setLogin("e");
+// speakersRepository.insertSpeaker(a0);
+//listenersRepository.insertListener(a1);
+  //         listenersRepository.insertListener(a2);
         // listenersRepository.insertListener(a3);
         int aa= listenersRepository.getAllListeners().size()+speakersRepository.getAllSpeakers().size();
         //int a9= admRepository.getAllAdmins().size();
