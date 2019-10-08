e477f83d94c58f8cb16ff322326c799e8754d048
ddenouement <julialex38@gmail.com>
2019-10-08 21:15:07 +0300

    v2

--- src/main/java/com/meetup/controller/SpeakerController.java
+++ src/main/java/com/meetup/controller/SpeakerController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -31,6 +32,11 @@ public class SpeakerController {
     public ResponseEntity<List<Topic>> getAvailableTopics() {
         return new ResponseEntity<>(userService.getAllTopics(), HttpStatus.OK);
     }
+    
+    @GetMapping(value="/test")
+    public String currentUserName(Principal principal) {
+        return principal.getName();
 
+    }
 
 }
