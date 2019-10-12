887ed03db12ed7741724170d2e2b4b6ac89a1862
Dmytro <play4power@gmail.com>
2019-10-12 19:50:17 +0300

    Check-style errors reduced to 97

--- server/src/main/java/com/meetup/service/RoleProcessor.java
+++ server/src/main/java/com/meetup/service/RoleProcessor.java
@@ -3,10 +3,25 @@ package com.meetup.service;
 import com.meetup.entities.User;
 import java.util.List;
 
+/**
+ * Service, used to manage user roles.
+ */
+public final class RoleProcessor {
 
-public class RoleProcessor {
+    /**
+     * Private constructor. (Utility class)
+     */
+    private RoleProcessor() {
 
-    public static boolean isSpeaker(User user) {
+    }
+
+    /**
+     * Returns true, if user is speaker.
+     *
+     * @param user User, used to check if it is speaker
+     * @return boolean
+     */
+    public static boolean isSpeaker(final User user) {
         List<String> roles = user.getRoles();
         for (String s : roles) {
             if (s.equals("SPEAKER")) {
