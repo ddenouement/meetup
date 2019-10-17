9e56d9efd83e7c53dee10ad420b3930753d6e3b1
Polina Shlepakova <polinashlepakova@gmail.com>
2019-10-17 20:44:31 +0300

    Set max Hikari pool size
    Signed-off-by: Polina Shlepakova <polinashlepakova@gmail.com>

--- server/src/main/java/com/meetup/controller/AuthorizationController.java
+++ server/src/main/java/com/meetup/controller/AuthorizationController.java
@@ -14,7 +14,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
