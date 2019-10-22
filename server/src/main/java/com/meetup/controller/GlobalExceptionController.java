d4285f1f26d4fbd8c2b5b4822bbf0f0f014807fb
Dmytro <play4power@gmail.com>
2019-10-22 13:29:34 +0300

    Fixed web mvc bug

--- server/src/main/java/com/meetup/controller/GlobalExceptionController.java
+++ server/src/main/java/com/meetup/controller/GlobalExceptionController.java
@@ -11,14 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Exception controller class.
  * Returns ResponseEntity object with status code.
  */
 @ControllerAdvice
-@EnableWebMvc
 public class GlobalExceptionController {
 
     /**
